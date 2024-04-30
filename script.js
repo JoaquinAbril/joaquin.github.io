cfad34c6c8b6701214be5af0bee841f55389dbc4
JoaquinAbril <144773493+JoaquinAbril@users.noreply.github.com>
2024-04-30 17:43:57 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
+document.querySelectorAll('a[href^="#"]').forEach(anchor => { // Selects all anchor elements with href attribute starting with #
+    anchor.addEventListener('click', function (e) { // Adds click event listener to each anchor element
+        e.preventDefault(); // Prevents default anchor behavior
 
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
+        document.querySelector(this.getAttribute('href')).scrollIntoView({ // Scrolls to the target element smoothly
+            behavior: 'smooth' // Specifies the scrolling behavior
         });
     });
 });
